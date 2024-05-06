cb15eeff46ca9ec92017c139d40618638e2e36c6
gokul3710 <gokulkrishnawaryar@gmail.com>
2024-05-06 13:15:18 +0530

    sagas pattern / inbox  - outbox pattern

--- apps/workflows-service/src/workflows-service.module.ts
+++ apps/workflows-service/src/workflows-service.module.ts
@@ -4,9 +4,12 @@ import { WorkflowsServiceController } from './workflows-service.controller';
 import { WorkflowsServiceService } from './workflows-service.service';
 import { WorkflowsModule } from './workflows/workflows.module';
 import { HealthModule } from './health/health.module';
+import { InboxModule } from './inbox/inbox.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
+    ScheduleModule.forRoot(), // 👈
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.POSTGRES_HOST,
@@ -19,6 +22,7 @@ import { HealthModule } from './health/health.module';
     }),
     WorkflowsModule,
     HealthModule,
+    InboxModule,
   ],
   controllers: [WorkflowsServiceController],
   providers: [WorkflowsServiceService],
